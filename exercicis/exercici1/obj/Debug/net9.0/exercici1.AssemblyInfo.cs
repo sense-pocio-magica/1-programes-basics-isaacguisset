ef2d64dd43f421382ee063d3d0f5ac9fe9ff8e2b
isaacguisset <guissetisaac@gmail.com>
2025-09-30 15:42:33 +0200

    Primer commit, primer exercici fet

--- exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
+++ exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033395b090265ceecdda678dd9c7d01c3b25de61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e066306233eeab860822a0848e539c4006d635")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
