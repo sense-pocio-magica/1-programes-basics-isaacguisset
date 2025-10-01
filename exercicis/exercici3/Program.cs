f0bf58a3703912ef6511cc18e3798df0e8a3011d
isaacguisset <guissetisaac@gmail.com>
2025-10-01 13:17:44 +0200

    he canviat un if/else per un estoc ? per tal de treure linies no necesàries al codi

--- exercicis/exercici3/Program.cs
+++ exercicis/exercici3/Program.cs
@@ -9,17 +9,9 @@ class Program
         var nom = "Ratolí ThunderX3";
         double preu = 35.99;
         bool estoc = false;
-        string estocverbose;
-        if (estoc == false)
-        {
-            estocverbose = "no disponible";
-        }
-        else
-        {
-            estocverbose = "disponible";
-        }
         int compres_ultim_any = 300;
+        string estocverbose = estoc ? "disponible" : "no disponible";
 
-        Console.WriteLine($"El producte {nom} amb preu {preu} el seu estat és: {estocverbose} i té {compres_ultim_any} compres l'últim any.");
+    Console.WriteLine($"El producte {nom} amb preu {preu} el seu estat és: {estocverbose} i té {compres_ultim_any} compres l'últim any.");
     }
 }
