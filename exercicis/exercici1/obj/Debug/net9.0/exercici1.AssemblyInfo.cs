2a82e12a91817a7e3ae76b6084e028adda8e3218
isaacguisset <guissetisaac@gmail.com>
2025-10-06 16:15:26 +0200

    nose ha fet coses rares el programa

--- exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
+++ exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465236bcabace7ec5555c08d2212226f6943e586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1c020de85c48eb1fed338b7e2e812b903bef94")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
